adcd54f12cec47ff15ac63b65868409e0fa7d86a
Kristjan Cuznar <kristjancuznar0@gmail.com>
2024-08-24 10:04:25 +0200

    Add NMS in ai-engine-cpu.

--- ai-engine-cpu/dev.py
+++ ai-engine-cpu/dev.py
@@ -2,11 +2,16 @@ import onnxruntime as rt
 import cv2
 import numpy as np
 from engine.pipes.object_detectors.base.lp import LicensePlateDetector
+from engine.pipes.number_plate_text_readers.base.ocr import Ocr
+
 import time
 from tqdm import tqdm
 
+
 # Read and preprocess the image
-raw_img = cv2.imread("data/4.png")
+image_path = "data/"
+image_name = '4.png'
+raw_img = cv2.imread(image_path+image_name)
 
 config = {}
 config['top_n'] = 5
@@ -14,21 +19,37 @@ config['threshold'] = 0.5
 net = LicensePlateDetector(config)
 net.load_model("engine/models/model-detect.onnx")
 
+ocr = Ocr()
+ocr.load_model("engine/models/model-ocr.onnx")
+ocr.init_label_converter()
+
+
 time_start = time.time()
-N=100
-for i in tqdm(range(N)):
-    image_data = net.preprocess(raw_img)
-    # Inference run using image_data as the input to the model 
-    detections = net.predict(image_data)
+
+image_data = net.preprocess(raw_img)
+# Inference run using image_data as the input to the model 
+detections = net.predict(image_data)
 
 
-    for i, detection in enumerate(detections):
-        x_min = int(detection.Left * raw_img.shape[1])
-        x_max = int(detection.Right * raw_img.shape[1])
-        y_max = int(detection.Bottom * raw_img.shape[0])
-        y_min = int(detection.Top * raw_img.shape[0])
-        confidence = detection.Confidence
+for i, detection in enumerate(detections):
+    x_min = int(detection.Left * raw_img.shape[1])
+    x_max = int(detection.Right * raw_img.shape[1])
+    y_max = int(detection.Bottom * raw_img.shape[0])
+    y_min = int(detection.Top * raw_img.shape[0])
+    confidence = detection.Confidence
+    
+    cropped_img = raw_img[y_min:y_max, x_min:x_max, :]
+    cropped_img = cv2.resize(cropped_img, (300,100))
+    cropped_img = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2RGB)
+    xs = ocr.preprocess([cropped_img])
+    license_plate_text = ocr.predict(xs)[0]
+    cv2.rectangle(raw_img, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+    cv2.putText(raw_img, license_plate_text, (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    img_name_parts = image_name.split(".")
+    cv2.imwrite(image_path+img_name_parts[0]+"_result."+img_name_parts[1], raw_img)
+    print(license_plate_text)
+                    
 time_end = time.time()
 
 working_time = time_end-time_start
-print(working_time/N)
+print(working_time)
--- ai-engine-cpu/engine/pipes/object_detectors/base/lp.py
+++ ai-engine-cpu/engine/pipes/object_detectors/base/lp.py
@@ -1,77 +1,68 @@
-
-
 import numpy as np
-from typing import List, Tuple, Any, Dict
-from engine.tools.image_processing import normalize_img
-from engine.tools.ocr_tools import StrLabelConverter, decode_batch
-import onnxruntime as rt 
 import cv2
+import onnxruntime as rt
 
-class LicensePlateDetector():
+
+class LicensePlateDetector:
     def __init__(self, config) -> None:
         self.input_name = None
         self.output_names = None
 
-        
         self.width = 300
         self.height = 300
-        self.color_channels  = 3
-        
+        self.color_channels = 3
+
         self.confidence_threshold = config['threshold']
         self.top_n = config['top_n']
-
-
 
     def load_model(self, engine_file_path):
         self.engine = rt.InferenceSession(engine_file_path) 
         self.input_name = self.engine.get_inputs()[0].name
-        # Get the output names
         self.output_names = [s.name for s in self.engine.get_outputs()]
         return self.engine
-    
-    
+
     def preprocess(self, img, need_preprocess=True):
         if need_preprocess:
             image_data = cv2.resize(img, (self.width, self.height)).astype(np.float32)
-        # Reorder dimensions to match model's expected input (1, channels, height, width)
-        image_data = np.transpose(image_data, (2, 0, 1))  # (height, width, channels) -> (channels, height, width)
-        # Add batch dimension: (1, channels, height, width)
+        image_data = np.transpose(image_data, (2, 0, 1))  
         image_data = np.expand_dims(image_data, axis=0)
         return image_data
 
-    def predict(self, xs: np.ndarray, return_acc: bool = False) -> Any:
-        
-        out = self.engine.run(self.output_names, {self.input_name: xs})
-
-        # Extract scores and boxes
-        scores = out[0]  # shape: (1, 3000, 2)
-        boxes = out[1]   # shape: (1, 3000, 4)
-        
-        # Squeeze the arrays to remove the batch dimension
-        scores = np.squeeze(scores, axis=0)
-        boxes = np.squeeze(boxes, axis=0)
-
+    def initial_filtering(self, scores, boxes):
         # Flatten the confidence scores and box coordinates
-        confidence_scores = scores[:, 1]  # Assuming the second value is the confidence score for the license plate class
+        confidence_scores = scores[:, 1]  
         boxes_above_threshold = boxes[confidence_scores > self.confidence_threshold]
         confidences_above_threshold = confidence_scores[confidence_scores > self.confidence_threshold]
 
         # Sort by confidence and select the top N detections
-        top_indices = np.argsort(confidences_above_threshold)[-self.top_n:][::-1]  # Sort descending and get top N indices
+        top_indices = np.argsort(confidences_above_threshold)[-self.top_n:][::-1]  
 
         top_boxes = boxes_above_threshold[top_indices]
         top_confidences = confidences_above_threshold[top_indices]
-        
+
+        return top_boxes, top_confidences
+
+    def predict(self, xs: np.ndarray):
+        out = self.engine.run(self.output_names, {self.input_name: xs})
+
+        # Extract scores and boxes
+        scores = np.squeeze(out[0], axis=0)  # shape: (3000, 2)
+        boxes = np.squeeze(out[1], axis=0)   # shape: (3000, 4)
+
+        # Perform initial filtering
+        top_boxes, top_confidences = self.initial_filtering(scores, boxes)
+
+        # Apply Non-Maximum Suppression (NMS)
+        final_boxes, final_confidences  = self.non_maximum_suppression(top_boxes, top_confidences)
+
         # Prepare the list of detection objects
         detections = []
-        for box, confidence in zip(top_boxes, top_confidences):
-            # Scale the coordinates to the original image size
+        for box, confidence in zip(final_boxes, final_confidences):
             x_min = box[0]
             y_min = box[1]
             x_max = box[2]
             y_max = box[3]
 
-            # Create a detection object (simulating the structure you expect)
             detection = type('Detection', (object,), {})()
             detection.Left = x_min
             detection.Right = x_max
@@ -82,3 +73,38 @@ class LicensePlateDetector():
             detections.append(detection)
         
         return detections
+
+    def non_maximum_suppression(self, boxes, confidences, iou_threshold=0.5):
+        if len(boxes) == 0:
+            return []
+
+        x1 = boxes[:, 0]
+        y1 = boxes[:, 1]
+        x2 = boxes[:, 2]
+        y2 = boxes[:, 3]
+
+        areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+        order = confidences.argsort()[::-1]
+
+        keep = []
+        while order.size > 0:
+            i = order[0]
+            keep.append(i)
+            xx1 = np.maximum(x1[i], x1[order[1:]])
+            yy1 = np.maximum(y1[i], y1[order[1:]])
+            xx2 = np.minimum(x2[i], x2[order[1:]])
+            yy2 = np.minimum(y2[i], y2[order[1:]])
+
+            w = np.maximum(0, xx2 - xx1 + 1)
+            h = np.maximum(0, yy2 - yy1 + 1)
+
+            inter = w * h
+            iou = inter / (areas[i] + areas[order[1:]] - inter)
+
+            indices_to_keep = np.where(iou <= iou_threshold)[0]
+            order = order[indices_to_keep + 1]
+
+
+        final_boxes = boxes[keep]
+        final_confidences = confidences[keep]
+        return final_boxes, final_confidences
